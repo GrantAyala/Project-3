cb6cc76a1ec2247b85c1666152e6057889d220ce
Grant Ayala <grant.ayala@gmail.com>
2025-08-24 13:22:13 -0400

    Initial commit

---
+++ tests/integration-custom/in/i-type-test.s
@@ -0,0 +1,20 @@
+.text
+addi t0 x0 10
+addi t1 x0 13
+addi t2 x0 -8
+
+addi a0 t0 2
+
+andi a0 t1 6
+
+ori a0 t0 7
+
+xori a0 t0 3
+
+slli a0 t1 2
+
+srli a0 t0 1
+
+srai a0 t2 2
+
+slti a0 t1 100
---
+++ tests/integration-custom/in/r-type-test.s
@@ -0,0 +1,21 @@
+.text
+addi t0 x0 10
+addi t1 x0 13
+addi t2 x0 -8
+addi t3 x0 2
+
+add a0 t0 t1
+
+and a0 t1 t0
+
+or a0 t0 t2
+
+xor a0 t0 t1
+
+sll a0 t1 t3
+
+slt a0 t1 t2
+
+mul a0 t0 t1
+
+sub a0 t1 t3
